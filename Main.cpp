5b76c4fab9e83ec638a11b03166ed40d013f5444
schnekenberg <gschnekenberg@gmail.com>
2024-09-12 22:32:35 -0300

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+/* JVIDA_Main.cpp
+Projeto Jogo da vida
+22/11/2023
+*/
+#include "JVIDA2_Model.cpp"
+#include "JVIDA2_Controller.cpp"
+
+
+int main (){
+	tamanhoMundo();
+	iniciaListas();
+	while (sair == 1){
+	menuPrincipal();
+	}
+}
